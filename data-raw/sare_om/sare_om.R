a706f961bf37fc686147122ad40c638d74c1cb06
vanichols <virginia.nichols@gmail.com>
2021-04-20 15:24:38 -0500

    update with fixed st om values

--- data-raw/sare_om/sare_om.R
+++ data-raw/sare_om/sare_om.R
@@ -8,27 +8,54 @@ library(readxl)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(stringr)
 library(usethis)
 
 # raw data ----------------------------------------------------------------
 
-datraw <- read_csv("data-raw/sare_om/VIRGINIA_NICHOLS_ISU_S0215-221.csv")
+datraw1 <- read_csv("data-raw/sare_om/VIRGINIA_NICHOLS_ISU_S0215-221.csv")
+datraw2 <- read_csv("data-raw/sare_om/VIRGINIA_NICHOLS_AMES_S0416-920.csv")
 
-dkey <- read_csv("data-raw/sare_texture/template_texture-msmts.csv") %>% select(-labeled) %>% mutate(Sample = textsamp_id)
+dkey1 <- read_csv("data-raw/sare_texture/template_texture-msmts.csv") %>% select(-labeled) %>% mutate(Sample = textsamp_id)
+dkey2 <- read_csv("data-raw/sare_texture/template_texture-msmts-new-Stout.csv") %>% select(-labeled) %>% mutate(Sample = textsamp_id)
 
 plotkey <- read_csv("data-raw/sare_plotkey/sare_plotkey.csv")
 
 # wrangle data ------------------------------------------------------------
 
-sare_om <-
-  datraw %>%
+sare_om1 <-
+  datraw1 %>%
   rename("textsamp_id" = samp_id) %>%
   select(-starts_with("x"), -date) %>%
-  left_join(dkey) %>%
+  left_join(dkey1) %>%
   janitor::clean_names() %>%
   select(code, ph, om, potassium, phos) %>%
   rename("plot_id" = code) %>%
   select(plot_id, everything())
+
+sare_om2 <-
+  datraw2 %>%
+  rename("textsamp_id" = samp_id) %>%
+  select(-starts_with("x"), -date) %>%
+  left_join(dkey2) %>%
+  janitor::clean_names() %>%
+  select(code, ph, om, potassium, phos) %>%
+  rename("plot_id" = code) %>%
+  select(plot_id, everything())
+
+sare_om1 %>%
+  filter(grepl("St", plot_id)) %>%
+  mutate(batch = "A") %>%
+  bind_rows(sare_om2 %>% mutate(batch = "B")) %>%
+  mutate(cctrt = str_sub(plot_id, -2, -1)) %>%
+  ggplot(aes(cctrt, om)) +
+  geom_jitter(aes(color = batch))
+
+sare_om <-
+  sare_om1 %>%
+  filter(!grepl("St", plot_id)) %>%
+  bind_rows(sare_om2)
+
 
 sare_om %>% write_csv("data-raw/sare_om/sare_om.csv")
 usethis::use_data(sare_om, overwrite = T)
@@ -36,7 +63,7 @@ usethis::use_data(sare_om, overwrite = T)
 
 # viz check ---------------------------------------------------------------
 
-dat %>%
+sare_om %>%
   left_join(plotkey) %>%
   select(field_id, sys_trt, site_name, cc_trt, ph:phos) %>%
   pivot_longer(ph:phos) %>%
